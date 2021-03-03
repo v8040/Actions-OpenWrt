8f01666d84865e576b1c1614198fc21ad119c032
v8040 <59680407+v8040@users.noreply.github.com>
2021-03-03 13:03:43 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
-
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+cd openwrt
+# wireless
+#rm -rf files/etc/config/wireless
+#rm -rf files/etc/modules.d/wireless_enable
+# Add luci-app-openclash
+git clone https://github.com/vernesong/OpenClash.git package-temp
+mv -f package-temp/luci-app-openclash package/lean/
+rm -rf package-temp
+# Add luci-theme-opentomcat
+git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git theme-temp/luci-theme-opentomcat
+rm -rf theme-temp/luci-theme-opentomcat/LICENSE
+rm -rf theme-temp/luci-theme-opentomcat/README.md
+mv -f theme-temp/luci-theme-opentomcat package/lean/
+rm -rf theme-temp
+default_theme='opentomcat'
+sed -i "s/bootstrap/$default_theme/g" feeds/luci/modules/luci-base/root/etc/config/luci
+# Add luci-app-vssr
+git clone https://github.com/jerrykuku/lua-maxminddb.git package-temp/lua-maxminddb
+git clone https://github.com/jerrykuku/luci-app-vssr.git package-temp/luci-app-vssr
+git clone https://github.com/kenzok8/small.git package-temp/small
+cp -r package-temp/small/* package/lean/
+mv -f package-temp/lua-maxminddb package/lean/
+mv -f package-temp/luci-app-vssr package/lean/
+rm -rf package-temp
